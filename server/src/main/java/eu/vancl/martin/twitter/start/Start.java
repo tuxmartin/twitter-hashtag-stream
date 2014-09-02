23fdd1b1dad1809745a94346920a4977d081bce0
Martin Vancl <tux.martin@gmail.com>
2014-09-02 23:40:44 +0200

    -UDP client (posilani tweetu na UDP server)
    Stejne se nepouzival...

--- server/src/main/java/eu/vancl/martin/twitter/start/Start.java
+++ server/src/main/java/eu/vancl/martin/twitter/start/Start.java
@@ -7,7 +7,6 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import eu.vancl.martin.twitter.service.TcpClient;
 import eu.vancl.martin.twitter.service.TwitterStreamFind;
-import eu.vancl.martin.twitter.service.UDPClient;
 
 /* ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! 
  * Before start app run
@@ -18,15 +17,12 @@ import eu.vancl.martin.twitter.service.UDPClient;
 public class Start {
 	static final Logger log = Logger.getLogger(Start.class); 	
 
-	//static UDPClient udpClient;
 	static TcpClient tcp;
 	
 	public static void main(String[] args) throws InterruptedException {
 		DOMConfigurator.configure("log4j.xml");
 
     	log.info("Starting application...");        
-
-        //udpClient = new UDPClient("127.0.0.1", 1234);
         
         tcp = new TcpClient(new Start());
         tcp.connect();
@@ -50,13 +46,11 @@ public class Start {
 	
 	public void sendTweet(String tweet) {				
 		// UTF-8 tweet:
-		//udpClient.send("\n" + tweet + "\n");		
 		//tcp.sendMessage("\n" + tweet + "\n");
 		
 		// ASCII tweet:
 		try {
 			byte[] asciiTweet = tweet.getBytes("UTF-8");
-			//udpClient.send("\n" + new String(asciiTweet, "US-ASCII") + "\n");
 			tcp.sendMessage("\n" + new String(asciiTweet, "US-ASCII") + "\n");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
